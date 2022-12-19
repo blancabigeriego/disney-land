11761b5828cf099740722f52af2b7b238fb312df
Blanca <blancabcbt@gmail.com>
2022-12-19 20:48:38 +0100

    create basic html for create page

--- src/app/characters/create-character/create-character.page.ts
+++ src/app/characters/create-character/create-character.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-character',
-  templateUrl: './create-character.page.html',
-  styleUrls: ['./create-character.page.scss'],
+  selector: "app-create-character",
+  templateUrl: "./create-character.page.html",
+  styleUrls: ["./create-character.page.scss"],
 })
 export class CreateCharacterPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
